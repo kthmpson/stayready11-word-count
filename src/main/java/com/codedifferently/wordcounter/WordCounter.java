4a6f5be323966e8569dac1a453ba1ccbce05e951
Kaiyen Thompson <kthompson6300@gmail.com>
2020-07-23 14:02:41 -0400

    not ready

--- src/main/java/com/codedifferently/wordcounter/WordCounter.java
+++ src/main/java/com/codedifferently/wordcounter/WordCounter.java
@@ -1,4 +1,26 @@
 package com.codedifferently.wordcounter;
+import java.util.*;
+import java.util.File;
+
 
 public class WordCounter {
+    HashMap<String,Integer> wordmap = new HashMap();
+    File text = new File ("TheModernPrometheus.txt");
+
+    Scanner scnr = new Scanner(text);
+    //file name
+    //initialize scanner
+        //read file
+    //while loop
+        //for loop to read each word
+    //write method with condition fir the first time seeing a word
+        //this is where i am going to use put method and my value will be one
+    // condition for the duplicate occurrence for word, increments go here
+   //create word count
+    //display aplha order
+    //create main at end
+    //read file test
+    //new word test
+    //duplicate word test
+
 }
--- src/test/java/com/codedifferently/wordcounter/WordCounterTest.java
+++ src/test/java/com/codedifferently/wordcounter/WordCounterTest.java
@@ -1,4 +1,20 @@
 package com.codedifferently.wordcounter;
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.HashMap;
 
 public class WordCounterTest {
-}
+    @Test
+    public void hashmapTest() {
+        //Given
+        WordCounter wc = new WordCounter();
+        int expected = 1;
+        //When
+        wc.wordmap.put("Kaiyen",1);
+        int actual = wc.wordmap.get("Kaiyen");
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    }
+
